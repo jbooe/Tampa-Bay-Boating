81286fda6e1218b5f4e3ab041afeedc9564a1bb9
Jason Booe <jbooe50@gmail.com>
2020-11-09 13:21:29 -0500

    JSON changes

--- src/shared/directoryItems.js
+++ src/shared/directoryItems.js
@@ -3,25 +3,25 @@ export const DIRECTORYLISTS = [
         id: 0,
         name: 'Tampa Bay Area Islands',
         description: 'Come and explore one of our many beautiful islands',
-        image: 'http://jbooe.github.io/TampaBayBoating/assets/images/island.jpg'
+        image: 'https://jbooe.github.io/TampaBayBoating/assets/images/island.jpg'
     },
     {
         id: 1,
         name: 'Sand Bars',
         description: 'A boating hazard, but a great place to park and relax',
-        image: 'http://jbooe.github.io/TampaBayBoating/assets/images/sandbar.jpg'
+        image: 'https://jbooe.github.io/TampaBayBoating/assets/images/sandbar.jpg'
     },
     {
         id: 2, 
         name: 'Local Boat Access Restaurants',
         description: 'Grab a bite to eat after a great day on the water',
-        image: 'http://jbooe.github.io/TampaBayBoating/assets/images/restaurant.jpg'
+        image: 'https://jbooe.github.io/TampaBayBoating/assets/images/restaurant.jpg'
     },
     {
         id: 3,
         name: 'The Local Wildlife',
         description: "You're gonna need a bigger boat",
-        image: 'http://jbooe.github.io/TampaBayBoating/assets/images/shark.jpg'
+        image: 'https://jbooe.github.io/TampaBayBoating/assets/images/shark.jpg'
     }
     
 ];
